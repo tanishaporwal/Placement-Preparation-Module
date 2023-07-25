42b62dc24d7329d29ac85a319bd37b0660399668
Tanisha Porwal <95023899+tanishaporwal@users.noreply.github.com>
2023-07-25 16:42:39 +0530

    Time: 17 ms (7.26%) | Memory: 14.1 MB (27.34%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -4,10 +4,10 @@ public:
         int x=0;
         for(int i=0; i<operations.size(); i++){
             if(operations[i]=="--X" ||operations[i]=="X--" ){
-                 x=x-1;
+                 x--;
             }
             else{
-                x=x+1;
+                x++;
             }
         }
         return x;
