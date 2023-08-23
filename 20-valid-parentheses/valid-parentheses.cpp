1fd3af6ccf0e5f0554f4290469b0ff250c24d91b
Tanisha Porwal <95023899+tanishaporwal@users.noreply.github.com>
2023-08-23 15:03:24 +0530

    Time: 3 ms (42.18%) | Memory: 6.4 MB (37.98%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       
+        if(s.length()==1){
+            return false;
+        }
+        stack<char>st;
+     
+        for(int i=0; i<s.length(); i++){
+            char ch=s[i];
+            if(ch=='(' ||ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else {
+                if(!st.empty()){
+                if(ch==')' && st.top()=='('){
+                    st.pop();
+                }
+                 else if(ch=='}' && st.top()=='{'){
+                    st.pop();
+                }
+                 else if(ch==']' && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                             return false;
+                }
+            }
+            else{
+                return false;
+            }}
+            
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
